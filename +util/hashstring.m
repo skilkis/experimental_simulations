c6d0d42d1afb1ecc7ed70cfc53ac7e72c6ad0fc2
skilkis <sankilkis@msn.com>
2021-03-25 12:20:24 +0100

    Use Java instead of .NET for SHA-1 hashing

--- +util/hashstring.m
+++ +util/hashstring.m
@@ -1,7 +1,24 @@
 function [hash] = hashstring(string)
 %HASHSTRING Hashes a string using SHA-1
-    c = convertStringsToChars(string); % Must be char to convert to uint8
-    hasher = System.Security.Cryptography.SHA1Managed; % .NET SHA-1 hasher
-    sha1 = dec2hex(uint8(hasher.ComputeHash(uint8(c))));
-    hash = convertCharsToStrings(sha1');
+%
+%AUTHOR: 
+%   Oliver Woodford
+%
+%USAGE:
+%
+%   hash = string2hash(string)
+%
+%IN:
+%   string - a string!
+%
+%OUT:
+%   hash - a 64 character string, encoding the SHA-1 hash of string
+c = convertStringsToChars(string); % Must be char to convert to uint8
+persistent md
+if isempty(md)
+    md = java.security.MessageDigest.getInstance('SHA-1');
+end
+hash = upper(...
+    sprintf('%2.2x', typecast(md.digest(uint8(c)), 'uint8')')...
+);
 end
