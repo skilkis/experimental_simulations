03233b64b0ffd11f3d163b1673935a44042c7cc1
skilkis <sankilkis@msn.com>
2021-03-28 18:53:20 +0200

    Renable mic and pressure data in main script

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ clc
 clear all
 close all
 
-% MicData = processing.mic.processdata(fullfile('data', 'MICS'));
+MicData = processing.mic.processdata(fullfile('data', 'MICS'));
 
 raw_balance_files = {...
     'raw_rudder0.txt',...
@@ -14,14 +14,15 @@ BalData = processing.bal.processdata(...
     raw_balance_files,...
     {'zer_ 20210303_edited.txt'}...
 );
-% PresData = processing.pres.processdata(fullfile('data', 'PRES'));
+PresData = processing.pres.processdata(fullfile('data', 'PRES'));
 
 % Applying Corrections
-[BalDataCorr,BalDataCorr_Indiv] = processing.correction.applycorrection(BalData);
-% % Saving results to data/processed.mat
-% cache_file = sprintf('%s.mat', fullfile('data', 'processed'));
-% [cache_path, ~, ~] = fileparts(cache_file);
-% if ~exist(cache_path, 'dir')
-%     mkdir(cache_path)
-% end
-% save(cache_file,'BalData', 'BalDataCorr', 'MicData', 'PresData');
+BalDataCorr = processing.correction.applycorrection(BalData);
+
+% Saving results to data/processed.mat
+cache_file = sprintf('%s.mat', fullfile('data', 'processed'));
+[cache_path, ~, ~] = fileparts(cache_file);
+if ~exist(cache_path, 'dir')
+    mkdir(cache_path)
+end
+save(cache_file,'BalData', 'BalDataCorr', 'MicData', 'PresData');
