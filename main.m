f388ebb37d4df62397ba0b5790695d17cbc07010
Anand0301 <vijayaraghavan.anand@gmail.com>
2021-03-28 04:28:42 +0200

    Update main.m
    receive multiple corrected data files

--- main.m
+++ main.m
@@ -1,4 +1,8 @@
-MicData = processing.mic.processdata(fullfile('data', 'MICS'));
+clc
+clear all
+close all
+
+% MicData = processing.mic.processdata(fullfile('data', 'MICS'));
 
 raw_balance_files = {...
     'raw_rudder0.txt',...
@@ -10,14 +14,14 @@ BalData = processing.bal.processdata(...
     raw_balance_files,...
     {'zer_ 20210303_edited.txt'}...
 );
-PresData = processing.pres.processdata(fullfile('data', 'PRES'));
+% PresData = processing.pres.processdata(fullfile('data', 'PRES'));
 
 % Applying Corrections
-BalDataCorr = processing.correction.applycorrection(BalData);
-% Saving results to data/processed.mat
-cache_file = sprintf('%s.mat', fullfile('data', 'processed'));
-[cache_path, ~, ~] = fileparts(cache_file);
-if ~exist(cache_path, 'dir')
-    mkdir(cache_path)
-end
-save(cache_file,'BalData', 'BalDataCorr', 'MicData', 'PresData');
+[BalDataCorr,BalDataCorr_Indiv] = processing.correction.applycorrection(BalData);
+% % Saving results to data/processed.mat
+% cache_file = sprintf('%s.mat', fullfile('data', 'processed'));
+% [cache_path, ~, ~] = fileparts(cache_file);
+% if ~exist(cache_path, 'dir')
+%     mkdir(cache_path)
+% end
+% save(cache_file,'BalData', 'BalDataCorr', 'MicData', 'PresData');
